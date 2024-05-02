5f3ed4c5136e3b8d8f45e18053fcaad9a2c48558
jfernandezcl <javierfernandez0987@gmail.com>
2024-05-02 18:50:34 +0200

    Ejercicio clicks contador

--- part1/src/index.jsx
+++ part1/src/index.jsx
@@ -3,48 +3,40 @@ import { useState } from "react";
 import './index.css'
 
 
-
-const App = (props) => {
-  const [contador, updateContador] = useState(8);
-  const handleClick = () => {
-    updateContador(contador + 1);
+const App = () => {
+  const [counters, setCounters] = useState ({
+    left: 0,
+    right: 0,
+    clicks: 0
+  })
+  const handleClickLeft = () => {
+    const newCountersState = {
+      ...counters,
+      left: counters.left + 1,
+      clicks: counters.clicks + 1,
+    }
+    setCounters(newCountersState);
   }
-  const handleClickReset = () => {
-    updateContador(0);
-  }
-  const isEven = contador % 2 === 0;
-  const mensajePar = isEven ? "Es número Par" : "Es número Impar";
-
+  const handleClickRight = () => {
+    setCounters ({
+      ...counters,
+      right: counters.right + 1,
+      clicks: counters.clicks + 1
+    });
+  };
   return (
-    <div className="contenedor">      
-      <p className="titulo">El valor de contador es:</p>
-      <h1 className="contador">{contador}</h1>
-      <div className="contenedor-mensajes">
-      <p className="mensaje">{mensajePar}</p>
+    <div className="container">
+      <p className="title">Contador de clicks</p>
+      <div className="container-button">
+      {counters.left}
+      <button className="button" onClick={handleClickLeft}>Left</button>
+      <button className="button" onClick={handleClickRight}>Right</button>
+      {counters.right}
       </div>
-      
-      <div className="button-container">
-        <button className="button"
-          onClick={handleClick}
-        >
-          Incrementar
-        </button>
-      </div>
-      <div className="button-reset-container">
-        <button className="button"
-          onClick={handleClickReset}
-        >
-          Resetear
-        </button>
-      </div>
-        
+      <p className="title">Clicks totales: {counters.clicks}</p>
     </div>
-  
-  );
+  )
 }
-
-
-
 
   ReactDOM.createRoot(document.getElementById('root')).render(  
     <App  />
